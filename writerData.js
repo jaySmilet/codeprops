22441134d41923dd9029b25776a90a6bd785f547
Jayganesh Gupta <36183760+jaySmilet@users.noreply.github.com>
2025-11-14 18:21:12 +0530

    updated for codevichar

--- writerData.js
+++ writerData.js
@@ -1,22 +1,22 @@
 const writers = {
   admin: {
-    name: 'Jayganesh',
+    name: 'Jayganesh Gupta',
     role: 'Admin',
-    photo: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEig1NzTuAl0v4I9oOLJ9zizxOFjD1U812KuYlq_1y3r9R-r51CQBYNpkTiVYqZj6TomAMcbh7nqeTewzWFWpFuGTRLH-VnBXjslcxaiIPtB0x3PRqHb7VuiMETfX59WgsSG2RjaKblnShCE-4f3OxV0a0K5rk1bs7D0u_yYhCHFHXOpuGM/s1600/1000030798.jpg',
+    photo: 'https://assets.codevichar.com/writers-pic/jayganesh-gupta.webp',
     profileUrl: 'https://www.linkedin.com/in/jayganesh-gupta'
   },
   rajat_gupta: {
     name: 'Rajat Gupta',
     role: 'Writer',
     photo: '',
-    profileUrl: 'https://writer1profile.link'
+    profileUrl: 'https://www.linkedin.com/in/rajat-gupta-37a463212'
   }
   // Add more writers here
 };
 
 const writerPosts = {
-  admin: ['4381421792996858996', '4546568252534693486',],
-  rajat_gupta: ['8094892859165856464', '3215585198392231583']
+  admin: ['9065388885961233741', '7350547149860411588', '45602419137120732', '916278593117035825','8488116656687112237'],
+  rajat_gupta: ['7761740006827282118']
   // Add more mappings here
 };
 
